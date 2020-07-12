d5f83694917c34eb28cc92172f835fa7e6f53718
Oliver <johnoliver021@gmail.com>
2020-07-12 10:13:36 -0300

    validando a data no backend

--- back-end/src/routes/appointments.routes.ts
+++ back-end/src/routes/appointments.routes.ts
@@ -1,18 +1,33 @@
 import { Router, request, response } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour,parseISO, isEqual } from 'date-fns';
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment  {
+    id: string;
+    provider: string;
+    date: Date;
+}
 
-appointmentsRouter.post('/',(request, response) => {
-    console.log(request.body)
-    
-    const{ provider, date} = request.body;
+const appointments : Appointment[] = [];
+
+appointmentsRouter.post('/', (request, response) => {
+    const { provider, date } = request.body;
+
+    const parsedDate = startOfHour(parseISO(date))
+
+    const findAppointmentInSameData = appointments.find(appointment => 
+        isEqual(parsedDate, appointment.date),
+    )
+
+    if (findAppointmentInSameData) {
+        return response.status(400).json(({ message: 'This appointment is already booked' }))
+    }
 
     const appointment = {
         id: uuid(),
         provider,
-        date
+        date: parsedDate
     }
     appointments.push(appointment)
     return response.json(appointment)
