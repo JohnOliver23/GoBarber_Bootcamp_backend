2b10306c2715cd775bb9fdcbe293283f5115f19d
Oliver <johnoliver021@gmail.com>
2020-07-17 22:10:29 -0300

    upload de imagens

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
@@ -9,8 +10,10 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/files', express.static(uploadConfig.directory));
+
 app.use(routes);
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333!');
+  console.log('Server started on port 3333!');
 });
